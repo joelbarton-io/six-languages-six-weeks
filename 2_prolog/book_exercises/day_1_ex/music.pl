4e1961d07888e50c2fff943c8cbe6a700ae23297
Joel Barton <joelbarton406@gmail.com>
2022-10-20 19:43:14 -0600

    [+] musical knowledge base

---
+++ 2_prolog/book_exercises/day_1_ex/music.pl
@@ -0,0 +1,23 @@
+% FACTS:
+
+plays(hendrix, guitar).
+plays(daft_punk, synth).
+plays(phoebe, my_heart_strings).
+plays(st_vincent, guitar).
+plays(flume, synth).
+plays(mura_masa, synth).
+plays(big_thief, my_heart_strings).
+plays(rob_moose, strings).
+plays(verzache, ukelele).
+plays(verzache, guitar).
+
+genre(hendrix, rock).
+genre(daft_punk, funk).
+genre(phoebe, indie).
+genre(st_vincent, rock).
+genre(flume, electronica).
+genre(mura_masa, electronica).
+genre(big_thief, indie).
+genre(rob_moose, indie).
+genre(verzache, indie).
+
